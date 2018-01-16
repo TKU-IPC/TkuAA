0e33e40efa471ce935904fbfe09e40dfd0c878e4
林昀龍 <cloud0822@gmail.com>
2018-01-16 16:22:01 +0800

    add tku auth lib by using aspnet idetity

---
+++ aspnetIdentity/TkuIpcAuth.AspNetIdentity/IpcMis/TkUser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNet.Identity;
+
+namespace TkuIpcAuth.AspNetIdentity {   
+    public partial class TkUser : IUser<string> {
+        public TkUser() { 
+            TkClaims = new List<TkClaim>();
+            TkUserLoginInfos = new List<TkUserLoginInfo>();
+            TkUserRoles = new List<TkUserRole>();
+        }
+
+        public string Id { get; set; }          //inherit from IUser, must be unique
+        public string UserName { get; set; }    //inherit from IUser, must be unique
+        public string RealName { get; set; }
+        public string PasswordHash { get; set; }
+        public string UserPswd { get; set; }
+        public string FutmDept { get; set; }
+        public string PhoneExt { get; set; }
+        public string UserDept { get; set; }
+        public string UserDeptInq { get; set; }
+
+        public virtual ICollection<TkClaim> TkClaims { get; set; }
+        public virtual ICollection<TkUserLoginInfo> TkUserLoginInfos { get; set; }
+        public virtual ICollection<TkUserRole> TkUserRoles { get; set; }
+    }
+}
